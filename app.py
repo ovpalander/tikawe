f75832cf788d6521fcf727619394ea4f10e787a0
Otto Palander <palander.ov@gmail.com>
2025-09-19 23:13:00 +0300

    Added CSS, bug fixes in templates, testing stuff in app.py.

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-from flask import render_template, request
-
+from flask import render_template, request, session
+from config import secret_key
 
 app = Flask(__name__)
 
+app.secret_key = secret_key 
+
 @app.route("/")
 def index():
-    #TODO
+    #session["user_id"] = "asd"
+    #session["username"] = "KEK"
+    del session["user_id"]
+    del session["username"]
+    return render_template("index.html")
---
+++ config.py
@@ -0,0 +1,1 @@
+secret_key = "03b9bf93efc55e5bbcce1cfaa7c0c218"
